5d4b02958fa2b3387017d00f3aaab2b443e4331e
samirdanmari <samirdanmari9@gmail.com>
2026-02-20 03:11:10 +0100

    now updating material and cdk

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatLegacyButtonModule as MatButtonModule} from '@angular/material/legacy-button';
+import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatLegacyCardModule as MatCardModule} from '@angular/material/legacy-card'
+import {MatCardModule} from '@angular/material/card';
 import {DragDropModule} from '@angular/cdk/drag-drop';
 const MaterialComponents = [
   MatButtonModule,

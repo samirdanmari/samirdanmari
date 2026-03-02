a8417a99b44d5e3ee067a6bc8ceaf5da8d5db359
samirdanmari <samirdanmari9@gmail.com>
2026-03-02 17:50:44 +0100

    updated

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -10,7 +10,7 @@ import { Router, RouterLink, RouterOutlet } from '@angular/router';
 export class NavigationComponent implements OnInit {
 
   constructor(private router: Router) { }
-   title : string = "Sameer";
+   title : string = "SAMEER.DEV";
 
   ngOnInit(): void {
   }
